61c027016d5fc657a66ccce3f8146115ca45b141
chandrabudi <blogc4954@gmail.com>
2020-01-17 10:47:22 +0700

    jj

---
+++ index.js
@@ -0,0 +1,4 @@
+function luas(p,l) {
+	return document.write('panjang : '+p+'<br/>'+'lebar :'+l+'<br/>'+'luas Persegi panjang :'+p*l)
+}
+luas(2,3)
---
+++ kedua.js
@@ -0,0 +1,4 @@
+function volumedanlsper(p,l,t){
+ return document.write('Panjang :'+ p + '<br/>' + 'Lebar :'+l+'<br/>'+
+ 'Tinggi : '+t +'<br/>'+'Volume Balok : '+ p*l*t +'<br/>' + 'Luas Permukaan Balok : ' + 2*(p*l+p*t+l*t))
+}volumedanlsper(8,5,2)
